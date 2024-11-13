dfae1c73eeff938c9f5381a3424589bd0fe9dfd2
koiuzmi-byte <koizumi@ndkgk.co.jp>
2024-11-13 17:13:55 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app　by小泉")
+st.title("🎈 My new app　by小泉\nＷＥＢアプリのテスト")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
